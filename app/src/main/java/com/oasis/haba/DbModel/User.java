c2b72e20df10c8d5d57ec0464515562b984cdbac
Joseph Mugo <donmwas007@gmail.com>
2018-08-20 14:46:59 +0300

    User Object

---
+++ app/src/main/java/com/oasis/haba/DbModel/User.java
@@ -0,0 +1,34 @@
+package com.oasis.haba.DbModel;
+
+public class User {
+    public static String TABLE_NAME = "UserNumber";
+    public static String USER_NUMBER = "PhoneNumber";
+    public static final String COLUMN_KEY = "key";
+
+    public String userID;
+    public String userName;
+    public String emailaddress;
+    public String phoneNumber;
+
+
+    // Default constructor required for calls to
+    // DataSnapshot.getValue(User.class)
+    public User() {
+    }
+
+    public User(String emailaddress, String userName,String userID) {
+
+        this.emailaddress = emailaddress;
+        this.userName = userName;
+        this.userID = userID;
+
+    }
+
+    //Store user phone number in local storage
+    public static final String CREATE_TABLE =
+            " CREATE TABLE " + TABLE_NAME + "("
+                    + COLUMN_KEY +  " VARCHAR(100) PRIMARY KEY,"
+                    + USER_NUMBER +  " VARCHAR(10) "
+                    + ")";
+
+}
